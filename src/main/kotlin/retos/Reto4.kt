e0708e40ea657b1943c015b13fa416b3faef0e5e
DevCrazz <saracorraless06@gmail.com>
2023-03-13 20:20:22 +0100

    exercise Reto4 not finished

---
+++ src/main/kotlin/retos/Reto4.kt
@@ -0,0 +1,45 @@
+package retos
+
+/*
+ * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+ * Podrás configurar generar contraseñas con los siguientes parámetros:
+ * - Longitud: Entre 8 y 16.
+ * - Con o sin letras mayúsculas.
+ * - Con o sin números.
+ * - Con o sin símbolos.
+ * (Pudiendo combinar todos estos parámetros entre ellos)
+ */
+
+class Reto4 {
+    fun generadorPass(lenght:Int = 8, mayus: Boolean = false, nums: Boolean = false, simb: Boolean = false){
+        val numRamdom = 33 .. 126
+        val lenghtPassword: Int = (8 .. 16).random()
+        var password = ""
+
+        println(lenght)
+        for (element in 0 .. lenght){ // add choice of char type
+            var charType = (0 .. 4).random()
+            if (charType == 0){
+                password += randomChar(97, 122)
+            }
+            if (charType == 1){
+                password += randomChar(65, 90)
+            }
+            if (charType == 2){
+                password += randomChar(48, 57)
+            }
+            if (charType == 3){
+                password += randomChar(33, 48) //TO-DO: (58-65) (91-97)
+            }
+
+
+        }
+        println(password)
+    }
+
+    fun randomChar(n1:Int, n2:Int): Char{
+        val numRamdom = (n1 .. n2).random()
+        return numRamdom.toChar()
+
+    }
+}
