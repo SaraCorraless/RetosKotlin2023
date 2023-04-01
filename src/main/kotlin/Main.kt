cbc0843548bb84c9e3e4b4b080b59e722f71b50d
DevCrazz <saracorraless06@gmail.com>
2023-04-01 22:23:24 +0200

    Changes of reto3 and add reto4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,5 @@ import retos.*
 fun main(args: Array<String>) {
     //Reto1().fizzBuzz()
     //Reto2().lenguajeHacker("Texto a traducir: ")
-    Reto4().generadorPass(12, mayus = false, nums = true, simb = false)
+    Reto3().generadorPass(7, mayus = false, nums = true, simb = false)
 }
