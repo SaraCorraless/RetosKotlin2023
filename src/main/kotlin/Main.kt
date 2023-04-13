643db3b65094b1b236791f7ba162200bc28288d0
DevCrazz <saracorraless06@gmail.com>
2023-04-14 00:20:11 +0200

    finished reto5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,6 @@ fun main(args: Array<String>) {
     //Reto1().fizzBuzz()
     //Reto2().lenguajeHacker("Texto a traducir: ")
     //Reto3().generadorPass(7, mayus = false, nums = true, simb = false)
-    Reto4().resultReto4(4)
+    //Reto4().resultReto4(4)
+    Reto5().game(arrayOf("Piedra, Papel", "spock, tijera", "lagarto, spock"))
 }
--- src/main/kotlin/retos/Reto5.kt
+++ src/main/kotlin/retos/Reto5.kt
@@ -12,4 +12,57 @@ package retos
  */
 
 class Reto5 {
+    fun game(combinaciones: Array<String>){
+        var p1 = 0
+        var p2 = 0
+        var punto:Boolean = true
+        for (i in combinaciones.indices){
+            var otP1 = combinaciones[i].split(", ")[0]
+            var otP2 = combinaciones[i].split(", ")[1]
+            when(otP1.lowercase()){
+                "piedra"-> punto = piedra(otP2)
+                "papel"-> punto = papel(otP2)
+                "tijera"-> punto = tijera(otP2)
+                "lagarto"-> punto = lagarto(otP2)
+                "spock"-> punto = spock(otP2)
+            }
+            if (punto){
+                p1++
+            }else{
+                p2++
+            }
+
+        }
+
+        if (p1 == p2){
+            println("Tier")
+        }else{
+            if(p1 > p2){
+                println("Player1")
+            }else{
+                println("Plryer2")
+            }
+        }
+
+    }
+
+    fun piedra(option:String): Boolean{ var resp: Boolean
+        return option.lowercase() == "spock" || option.lowercase() == "tijeras"
+    }
+
+    fun papel(option:String): Boolean{
+        return option.lowercase() == "spock" || option.lowercase() == "tijeras"
+    }
+
+    fun tijera(option:String): Boolean{
+        return option.lowercase() == "lagarto" || option.lowercase() == "papel"
+    }
+
+    fun spock(option:String): Boolean{
+        return option.lowercase() == "tijera" || option.lowercase() == "lagarto"
+    }
+
+    fun lagarto(option:String): Boolean{
+        return option.lowercase() == "spock" || option.lowercase() == "papel"
+    }
 }
